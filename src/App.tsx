dfa6989392a9dd3f9b96e423262e4ead90211b14
PhairardDev <phairard@hotmail.com>
2024-10-07 23:05:40 +0700

    update useForm

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Refine, WelcomePage } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
 import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
+import { ListProducts } from "./pages/products/list";
+import { CreateProduct } from "./pages/products/create";
 
 function App() {
   return (
     <Refine dataProvider={dataProvider}>
-      <ShowProduct />
+      {/* <ShowProduct /> */}
+      <EditProduct />
+      {/* <CreateProduct /> */}
+      {/* <ListProducts /> */}
     </Refine>
   );
 }
